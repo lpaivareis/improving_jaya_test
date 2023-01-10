eeff2d984ec89a292ce393d36285f374c500a4bc
lpaivareis <lucasfelipedepaiva2@gmail.com>
2023-01-10 14:02:20 -0300

    implement basic auth in issues endpoint

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
-
 class IssuesController < ApplicationController
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  before_action :http_basic_authenticate, only: :index
+
   def index
     @issues = Issue.all
 
-    render json: @issues, status: :ok
+    if @issues.empty?
+      render json: { message: "No issues found" }, status: :not_found
+    else
+      render json: @issues, status: :ok
+    end
+  end
+
+  private
+
+  def http_basic_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "xxx" && password == "xxx"
+    end
   end
 end
--- spec/requests/issues_spec.rb
+++ spec/requests/issues_spec.rb
@@ -8,19 +8,42 @@ RSpec.describe "Issues" do
       create_list(:issue, 10)
     end
 
+    let(:headers) do
+      {
+        Authorization: ActionController::HttpAuthentication::Basic.encode_credentials("xxx", "xxx")
+      }
+    end
+
     it "returns http success" do
-      get issues_path
+      get issues_path, headers: headers
       expect(response).to have_http_status(:success)
     end
 
     it "returns all issues" do
-      get issues_path
+      get issues_path, headers: headers
       expect(response.body).to eq(Issue.all.to_json)
     end
 
     it "returns 10 issues" do
-      get issues_path
+      get issues_path, headers: headers
       expect(JSON.parse(response.body).size).to eq(10)
+    end
+
+    it "returns a 404 if no issues are found" do
+      Issue.destroy_all
+      get issues_path, headers: headers
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns a message if no issues are found" do
+      Issue.destroy_all
+      get issues_path, headers: headers
+      expect(JSON.parse(response.body)["message"]).to eq("No issues found")
+    end
+
+    it "returns a 401 if no credentials are provided" do
+      get issues_path
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
